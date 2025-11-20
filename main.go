d8af46a2f012befd1a8013e37495c3403bb99801
Maxim <golchanskiy23@gmail.com>
2025-11-20 18:00:00 +0300

    Configuration implementation

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	//"Service-for-assigning-reviewers-for-Pull-Requests/internal/server"
 	"Service-for-assigning-reviewers-for-Pull-Requests/config"
+	"Service-for-assigning-reviewers-for-Pull-Requests/internal/app"
 	logger "Service-for-assigning-reviewers-for-Pull-Requests/pkg/logger"
 	"os"
 )
 
 func main() {
-	// config
-	// logger
 	l := logger.SetupLogger()
-	if err := config.Configure(); err != nil {
+	if err := config.SystemVarsInit(); err != nil {
 		l.Error("failed to load .env file", "err", err)
 		os.Exit(1)
 	}
-	l.Info("correctly loaded .env file")
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		l.Error("failed to configuration", "err", err)
+		os.Exit(1)
+	}
+
+	if err = app.Run(cfg); err != nil {
+		l.Error("Error running app: %v", err)
+		os.Exit(1)
+	}
 
 	//srv := server.NewHTTPServer(":8080")
 	//srv.Run()
