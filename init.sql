10749d095476f25465c98674c13fa895a47448ba
Maxim <golchanskiy23@gmail.com>
2025-11-20 09:00:00 +0300

    Create tables file implementation

---
+++ init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE teams (
+    team_name TEXT PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    username TEXT NOT NULL,
+    team_name TEXT NOT NULL REFERENCES teams(team_name) ON DELETE RESTRICT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE pull_requests (
+    pull_request_id TEXT PRIMARY KEY,
+    pull_request_name TEXT NOT NULL,
+    author_id TEXT NOT NULL REFERENCES users(user_id) ON DELETE RESTRICT,
+    status TEXT NOT NULL CHECK (status IN ('OPEN','MERGED')) DEFAULT 'OPEN',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    merged_at TIMESTAMPTZ NULL
+);
+
+CREATE TABLE pr_reviewers (
+    pull_request_id TEXT NOT NULL REFERENCES pull_requests(pull_request_id) ON DELETE CASCADE,
+    reviewer_id TEXT NOT NULL REFERENCES users(user_id) ON DELETE RESTRICT,
+    assigned_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (pull_request_id, reviewer_id)
+);
