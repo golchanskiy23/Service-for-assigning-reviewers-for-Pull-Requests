cf2fffa311ac393922bbcd618b5303611db5f1b3
Maxim <golchanskiy23@gmail.com>
2025-11-21 02:20:00 +0300

    Configuration implementation

--- config/config.go
+++ config/config.go
@@ -2,12 +2,50 @@ package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"time"
 )
 
-func Configure() error {
-	if err := godotenv.Load(".env.example"); err != nil {
-		return fmt.Errorf("error loading .env file: %v", err)
+type Config struct {
+	App      App        `mapstructure:"app"`
+	Server   HttpServer `mapstructure:"server"`
+	Database DB         `mapstructure:"database"`
+}
+
+type App struct {
+	Name    string `mapstructure:"name"`
+	Version string `mapstructure:"appversion"`
+}
+
+type HttpServer struct {
+	ReadTimeout     *time.Duration `mapstructure:"read_timeout"`
+	WriteTimeout    *time.Duration `mapstructure:"write_timeout"`
+	ShutdownTimeout time.Duration  `mapstructure:"shutdown_timeout"`
+}
+
+type DB struct {
+	Name        string `mapstructure:"name"`
+	Port        int    `mapstructure:"port"`
+	SSLMode     Mode   `mapstructure:"SSLMode"`
+	Schema      string `mapstructure:"schema"`
+	MaxPoolSize int    `mapstructure:"MaxPoolSize"`
+}
+
+type Mode string
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("fatal error config file: %s", err)
 	}
-	return nil
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("marshaling error: %s", err)
+	}
+	return cfg, nil
 }
